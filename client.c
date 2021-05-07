74158272a7811ec0bcc28a871808be46b2102173
Augusto Alonso <augustoalonso98@gmail.comn>
2021-05-07 12:21:58 -0600

    lol

--- client.c
+++ client.c
@@ -98,8 +98,8 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    char *ip = "127.0.0.1";
     int port = atoi(argv[1]);
+    char *ip = "13.58.87.181";
 
     signal(SIGINT, catch_ctrl_c_and_exit);
 
--- server.c
+++ server.c
@@ -195,7 +195,7 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    char *ip = "127.0.0.1";
+    char *ip = "13.58.87.181";
     int port = atoi(argv[1]);
     int option = 1;
     int listenfd = 0, connfd = 0;
