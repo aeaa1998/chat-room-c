47c69636d42711e29c3af1fb6f3f2989b91dfe2d
Augusto Alonso <augustoalonso98@gmail.comn>
2021-05-07 16:13:29 -0600

    lol

--- server.c
+++ server.c
@@ -157,7 +157,7 @@ void send_message(char *mess, int uid)
             counter++;
         }
         char real_message[strlen(mess) - (2 + counter)];
-        strncpy(real_message, &mess[2 + counter - 1], strlen(mess));
+        strncpy(real_message, mess[2 + counter - 1], strlen(mess) - 2 + counter);
     }
     else
     {
