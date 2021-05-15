eebb69a1cd827bde6c7fe5a490e24d73725f95c7
Augusto Alonso <augustoalonso98@gmail.comn>
2021-05-15 17:57:22 -0600

    lol

--- server.cpp
+++ server.cpp
@@ -47,7 +47,7 @@ int get_client_index(string name)
     {
         if (clients[i])
         {
-            if (clients[i]->name.compare(name) == 0)
+            if (strcmp(clients[i]->name, name.c_str()) == 0)
             {
                 return i;
             }
@@ -256,7 +256,7 @@ void send_message(char *mess, int uid)
                 if (strcmp(clients[i]->name, payload.sender().c_str()) == 0)
                 {
                     found = 1;
-                    int if (write(clientToSend->socket_d, out.c_str(), out.length()) < 0)
+                    if (write(clientToSend->socket_d, out.c_str(), out.length()) < 0)
                     {
                         send_error_message("No se pudo obtener la informacion del usuario deseado.", sender_index);
                         break;
